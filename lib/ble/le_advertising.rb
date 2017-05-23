43fcab6c994dfa79b2114b66b9b54ad25f96d505
Fred Mitchell SB <frederick.mitchell@sensorberg.com>
2017-05-23 18:30:27 +0200

    a most curious thing

--- lib/ble/le_advertising.rb
+++ lib/ble/le_advertising.rb
@@ -1,7 +1,19 @@
 module BLE
   # Set up LE Advertising
   # https://kernel.googlesource.com/pub/scm/bluetooth/bluez/+/refs/heads/master/doc/advertising-api.txt
-  class LEAdvertising
-    
+  class LEAdvertising < DBus::Object
+    dbus_interface I_LE_ADVERTISING do
+      dbus_method :Release do |device|
+      end
+
+      #(object advertisement, dict options)
+      dbus_method :RegisterAdvertisement do 
+      end
+      
+      #(object advertisement)
+      dbus_method :UnregisterAdvertisement do        
+      end
+      
+    end
   end
 end
